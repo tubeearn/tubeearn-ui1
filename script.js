51d5312eb07d972e76858b2ec666d47d7ac1adfb
tubeearn <wwwsanjeevbarh95@gmail.com>
2025-07-23 21:47:20 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function search() {
+  alert("Search clicked");
+}
+function notify() {
+  alert("Notifications clicked");
+}
+function profile() {
+  alert("Profile clicked");
+}
+function navigate(section) {
+  alert("Navigating to: " + section);
+}
